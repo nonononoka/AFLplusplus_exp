9df7381b49ee77eac284419384d34bf490aafb8f
nonononoka <onon.kaori@gmail.com>
2025-11-28 00:02:33 +0900

    テスト用のコミットを修正

--- src/afl-fuzz-stats.c
+++ src/afl-fuzz-stats.c
@@ -614,8 +614,8 @@ void maybe_update_plot_file(afl_state_t *afl, u32 t_bytes, double bitmap_cvg,
           (u32)afl->san_binary_length);                    /* ignore errors */
   
   if (!afl->saturation_level){
-    if (afl->plot_prev_ed - afl->last_updated_execs >= ave_execs_per_s*300){
-      if (((double)(t_bytes - afl->prev_total_edge_found_in_15_minutes) / (double)afl->prev_total_edge_found_in_15_minutes) <= 1000){
+    if (afl->plot_prev_ed - afl->last_updated_execs >= ave_execs_per_s*900){
+      if (((double)(t_bytes - afl->prev_total_edge_found_in_15_minutes) / (double)afl->prev_total_edge_found_in_15_minutes) <= 0.01){
         afl->saturation_level = 1;
         afl->last_updated_time = delta;
         ACTF("has saturated!");
@@ -626,7 +626,7 @@ void maybe_update_plot_file(afl_state_t *afl, u32 t_bytes, double bitmap_cvg,
   }
   else{
     // とりあえず3時間ごとにsaturation_levelをあげていくかあ
-    if(delta - afl->last_updated_time >= 600000){
+    if(delta - afl->last_updated_time >= 10800000){
       afl->saturation_level++;
       afl->last_updated_time = delta;
       ACTF("current saturation level: %u", afl->saturation_level);
